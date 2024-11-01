0cc69608000048f7c496b2a83432fb5ed42fe0cc
Hossam <hossamsoliuman@gmail.com>
2024-11-01 17:40:13 +0200

    add country and gender fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('user_name')->nullable();
             $table->string('account_id')->nullable();
             $table->string('phone')->nullable();
+            $table->string('country')->nullable();
+            $table->string('gender')->nullable();
             $table->integer('age')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
