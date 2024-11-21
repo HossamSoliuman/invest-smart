3cca2a903212399808bbc5faf858b2c63e86d16a
Hossam <hossamsoliuman@gmail.com>
2024-11-21 22:25:27 +0200

    add recaptcha on send and check code endpoints

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -7,6 +7,7 @@ use App\Mail\Verification;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
 use Nette\Utils\Random;
 
@@ -24,6 +25,17 @@ class VerificationController extends Controller
         //     return $this->apiResponse(null, 'Please wait a minute after the last mail sent', 0);
         // }
 
+        $validated = $request->validate([
+            'recaptcha' => 'required|string',
+        ]);
+
+        $recaptchaToken = $validated['recaptcha'];
+        $response = $this->validateRecaptcha($recaptchaToken);
+        $isValidRecaptcha = $response->json()['success'] ?? false;
+        if (!$isValidRecaptcha) {
+            return $this->apiResponse(null, 'Invalid reCAPTCHA token', 0);
+        }
+
         $code = random_int(100000, 999999);
         $user->email_verification_code = $code;
         $user->increment('email_verification_count');
@@ -35,19 +47,43 @@ class VerificationController extends Controller
         return $this->apiResponse(null, 'Sent successfully');
     }
 
-
     public function check(Request $request)
     {
-        $user = $request->user();
+        $validated = $request->validate([
+            'code' => 'required|integer',
+            'recaptcha' => 'required|string',
+        ]);
 
-        if (!$user->email_verification_code || $request->code != $user->email_verification_code) {
+        $recaptchaToken = $validated['recaptcha'];
+        $response = $this->validateRecaptcha($recaptchaToken);
+        $isValidRecaptcha = $response->json()['success'] ?? false;
+        if (!$isValidRecaptcha) {
+            return $this->apiResponse(null, 'Invalid reCAPTCHA token', 0);
+        }
+
+        $user = $request->user();
+        if (!$user->email_verification_code || $validated['code'] != $user->email_verification_code) {
             return $this->apiResponse(null, 'The code you entered is wrong', 0);
         }
 
         $user->email_verified_at = now();
-        $user->email_verification_code = null; 
+        $user->email_verification_code = null;
         $user->save();
 
         return $this->apiResponse(null, 'Verified');
+    }
+    
+    function validateRecaptcha($recaptchaToken)
+    {
+        $url = "https://www.google.com/recaptcha/api/siteverify";
+
+        $body = [
+            'secret' => env('RECAPTCHA_SECRET'),
+            'response' => $recaptchaToken
+        ];
+
+        $response = Http::asForm()->post($url, $body);
+
+        return  $response;
     }
 }
