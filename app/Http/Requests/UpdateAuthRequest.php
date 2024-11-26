2c8963791d7c03400e66cd6f7ae08ea6e4818b21
Hossam <hossamsoliuman@gmail.com>
2024-11-26 18:02:14 +0200

    make email unique

--- app/Http/Requests/UpdateAuthRequest.php
+++ app/Http/Requests/UpdateAuthRequest.php
@@ -6,26 +6,16 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateAuthRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
     public function rules()
     {
         return [
             'password' => 'nullable|string|min:8',
-            'email' => 'nullable|string|min:8',
+            'email' => 'nullable|string|email|unique:users,email,' . $this->user()->id,
             'name' => 'nullable|string|max:255',
             'phone' => 'nullable|string|max:25',
             'country' => 'nullable|string|max:255',
